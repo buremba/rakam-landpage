812bf2e81e5fccac347969382b969cd580b0444b
Cadabra <sboh@cadabra.studio>
2017-11-06 10:23:34 +0200

    Main page:  replacement of backgrounds  updated responsive  updated slider  replacement of texts

--- js/controllers.js
+++ js/controllers.js
@@ -459,3 +459,90 @@ app.controller('mainController', ['$scope', '$http', '$interval', function ($sco
     .controller('apiTabs', function ($scope, $window) {
 
     })
+
+
+    .controller('CarouselCtrl', function ($scope) {
+      $scope.myInterval = 3000;
+      $scope.noWrapSlides = false;
+      $scope.active = 0;
+      var slides = $scope.slides = [];
+      var currIndex = 0;
+
+      $scope.slides = [
+        { 
+          id: 0,
+          image:'/images/kaan_ugurlu.jpg',
+          altImage:'Kaan Uğurlu',
+          title:'Kaan Uğurlu',
+          title2:'Co-Founder | Scorp',
+          text:`With Rakam.io we're able to process our custom event data with ease. Being able to write our own custom reports allows us to created detailed reports. We can segment users based on complicated action history and see user engagements and retention for each user segment. The rakam BI panel allows us to create useful reports with graphs and tables easily which saves us the trouble of developing extra analytics panel.`
+        },
+        { 
+          id: 1,
+          image:'/images/bitmap.png',
+          altImage:'Kevin Adams',
+          title:'Kevin Adams',
+          title2:'CTO | Adventure Bucket List',
+          text:'“Rakam helped me get up and running with funnel / retention analysis in under an hour.”'
+        },
+        { 
+          id: 2,
+          image:'/images/bitmap2.png',
+          altImage:'Tomi Ayorinde',
+          title:'Tomi Ayorinde',
+          title2:'CEO | Mobileforms',
+          text:'We needed an Analytics solution that could scale with our business model. With rakam have a simple but yet powerful infrastructure that does that and more. I like that rakam does the heavy work for us. We don’t have to deal with multiple and complex Analytics products. We can just use Rakam and it grows with us.”'
+        }
+      ];
+
+
+        $scope.addSlide = function() {
+            var newWidth = 600 + slides.length + 1;
+            slides.push({
+              image: '//unsplash.it/' + newWidth + '/300',
+              text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+              id: currIndex++
+            });
+          };
+
+          $scope.randomize = function() {
+            var indexes = generateIndexesArray();
+            assignNewIndexesToSlides(indexes);
+          };
+
+          for (var i = 0; i < 4; i++) {
+            $scope.addSlide();
+          }
+
+          // Randomize logic below
+
+          function assignNewIndexesToSlides(indexes) {
+            for (var i = 0, l = slides.length; i < l; i++) {
+              slides[i].id = indexes.pop();
+            }
+          }
+
+          function generateIndexesArray() {
+            var indexes = [];
+            for (var i = 0; i < currIndex; ++i) {
+              indexes[i] = i;
+            }
+            return shuffle(indexes);
+          }
+
+          // http://stackoverflow.com/questions/962802#962890
+          function shuffle(array) {
+            var tmp, current, top = array.length;
+
+            if (top) {
+              while (--top) {
+                current = Math.floor(Math.random() * (top + 1));
+                tmp = array[current];
+                array[current] = array[top];
+                array[top] = tmp;
+              }
+            }
+
+            return array;
+          }
+        });
